d50458be489bcec4d64264e3a9933d53c93ea00e
Frederik Krämer <frdrkkraemer@gmail.com>
2024-09-23 05:39:01 +0200

    fff

--- src/pyside_config/__init__.py
+++ src/pyside_config/__init__.py
@@ -1,11 +1,12 @@
+import functools
 import typing as t
 
 import attrs
 from PySide6 import QtWidgets
 
-from .base import ConfigBase, WidgetPropertiesBase
+from .base import ConfigBase, WidgetPropertiesBase, update_qsettings
 
-__all__ = ["ConfigBase", "WidgetPropertiesBase", "EditorWidgetInfo"]
+__all__ = ["ConfigBase", "WidgetPropertiesBase", "EditorWidgetInfo", "config"]
 
 
 @attrs.define
@@ -15,3 +16,6 @@ class EditorWidgetInfo[W: QtWidgets.QWidget]:
     sig_value_changed: str
     set_value_method: str
     widget_properties: WidgetPropertiesBase[W] | None = None
+
+
+config = functools.partial(attrs.define, on_setattr=update_qsettings)
